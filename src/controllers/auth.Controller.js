45d2887fc828717e8ffed556f313dfc1e6bf7c67
AdnanM50 <a-19150@mangrove.edu.bd>
2024-09-02 21:31:29 +0600

    almost done

--- src/controllers/auth.Controller.js
+++ src/controllers/auth.Controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { generateTokenAndSetCookie } from "../utils/genarateTokenAndsetCookie.js";
-import { sendevarificationmail } from "../mailtrap/email.js";
+// import { sendevarificationmail } from "../mailtrap/email.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -30,12 +30,10 @@ export const signup = async (req, res) => {
     });
     await user.save();
     generateTokenAndSetCookie(res, user._id);
-    //jwt
-
     // await sendevarificationmail(user.email, varificationToken);
-    console.log("Starting email send process");
-await sendevarificationmail(user.email, varificationToken);
-console.log("Email send process completed");
+    // console.log("Starting email send process");
+    // await sendevarificationmail(user.email, varificationToken);
+    // console.log("Email send process completed");
 
     // res.status(201).json({
     //   success: "true",
@@ -46,22 +44,85 @@ console.log("Email send process completed");
     //   },
     // });
     res.status(201).json({
-        success: "true",
-        message: "User created successfully",
-        user: {
-          email: user.email,
-          name: user.name,
-          // Exclude password and other sensitive info
-        },
-      });
+      success: "true",
+      message: "User created successfully",
+      user: {
+        email: user.email,
+        name: user.name,
+        // Exclude password and other sensitive info
+      },
+    });
   } catch (error) {
     console.log(error);
     res.status(400).json({ success: "false", message: "Something went wrong" });
   }
 };
+
+export const varifyemail = async (req, res) => {
+  const { varificationToken } = req.body;
+  try {
+    if (!varificationToken) {
+      return res.status(400).json({ message: "Please provide a varification token" });
+    }
+    const user = await User.findOne({ varificationToken });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+    if (user.varificationToken !== varificationToken) {
+      return res.status(400).json({ message: "Invalid varification token" });
+    }
+    if (user.varificationExpiresAt < Date.now()) {
+      return res.status(400).json({ message: "Varification token expired" });
+    }
+    user.isvarified = true;
+    await user.save();
+    res.status(200).json({
+      success: "true",
+      message: "User varified successfully",
+      user: {
+        email: user.email,
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: "false", message: "Something went wrong" });
+  }
+};
+
+
 export const login = async (req, res) => {
-  res.send("Signup route");
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+    const user = await User.findOne ({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    generateTokenAndSetCookie(res, user._id);
+    res.status(200).json({
+      success: "true",
+      message: "User logged in successfully",
+      user: {
+        email: user.email,
+        name: user.name,
+        // Exclude password and other sensitive info
+      },
+    });
+  }
+  catch (error) {
+    console.log(error);
+    res.status(400).json({ success: "false", message: "Something went wrong" });
+  }
+     
 };
 export const logout = async (req, res) => {
-  res.send("Signup route");
+  res.clearCookie("token").json({ message: "Logged out successfully" });
+  
 };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { signup, login, logout } from '../controllers/auth.Controller.js';
+import { signup, login, logout, varifyemail } from '../controllers/auth.Controller.js';
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/varify-email", varifyemail);
 
 router.post('/login',login);
 
 router.post('/logout', logout);
 
-// Optional: For testing purposes, you can add a GET route
+//  For testing purposes, you can add a GET route
 router.get('/signup', (req, res) => {
     res.send('This is a GET request to signup. Use POST to actually sign up.');
 });
